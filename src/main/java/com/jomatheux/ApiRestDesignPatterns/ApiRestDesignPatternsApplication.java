c87a75e9fd16999b3a5499a1e527b5026e184458
jomatheux <jomatheux@gmail.com>
2024-09-07 12:15:58 -0300

    initial commit

---
+++ src/main/java/com/jomatheux/ApiRestDesignPatterns/ApiRestDesignPatternsApplication.java
@@ -0,0 +1,13 @@
+package com.jomatheux.ApiRestDesignPatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestDesignPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestDesignPatternsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jomatheux/ApiRestDesignPatterns/ApiRestDesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jomatheux.ApiRestDesignPatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestDesignPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
